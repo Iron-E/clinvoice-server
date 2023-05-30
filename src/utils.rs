2455bae2b33f90cd89a09cc23e3c9414ef0420d7
Iron-E <code.iron.e@gmail.com>
2023-05-29 23:04:03 -0400

    feat(utils): provide DB connection helper

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,10 @@
 use winvoice_schema::chrono::{DateTime, Datelike, Local, NaiveDateTime, TimeZone, Timelike, Utc};
+#[cfg(test)]
+use {
+	core::cell::OnceCell,
+	sqlx::{Database, Pool},
+	std::{env, fs, path::PathBuf},
+};
 
 /// Create a [`DateTime<Utc>`] out of some [`Local`] [`NaiveDateTime`].
 pub(crate) fn naive_local_datetime_to_utc(d: NaiveDateTime) -> DateTime<Utc>
@@ -9,15 +15,27 @@ pub(crate) fn naive_local_datetime_to_utc(d: NaiveDateTime) -> DateTime<Utc>
 		.into()
 }
 
+/// Connect to the database in the `DATABASE_URL` env variable.
+#[cfg(test)]
+pub(crate) async fn connect<Db>() -> Pool<Db>
+where
+	Db: sqlx::Database,
+{
+	// TODO: use `LazyCell`
+	static URL: OnceCell<String> = OnceCell::new();
+
+	Pool::<Db>::connect_lazy(URL.get_or_try_init(|| dotenvy::var("DATABASE_URL")).unwrap()).unwrap()
+}
+
 /// A temporary directory which can be used to write files into for `test`.
 #[cfg(test)]
-pub(crate) fn temp_dir(test: &str) -> std::path::PathBuf
+pub(crate) fn temp_dir(test: &str) -> PathBuf
 {
-	let mut parent = std::env::temp_dir();
+	let mut parent = env::temp_dir();
 	parent.push("winvoice-server");
 	parent.push(test);
 
-	std::fs::create_dir_all(&parent).unwrap();
+	fs::create_dir_all(&parent).unwrap();
 
 	parent
 }
