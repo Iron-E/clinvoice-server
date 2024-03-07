394cdc52bcdc2e47f99dd84cd815d3a2e02d779b
Iron-E <code.iron.e@gmail.com>
2024-03-06 19:38:41 -0500

    ci: dagger pipelines

---
+++ ci/scope.ts
@@ -0,0 +1,9 @@
+import { setTo } from '@iron-e/scabbard';
+import { CARGO_HACK_VERSION } from '@iron-e/scabbard/rust/scope/client';
+
+setTo('0.6.20', CARGO_HACK_VERSION);
+
+/**
+ * Default arguments for cargo hack.
+ */
+export const CARGO_HACK_ARGS = ['--feature-powerset', '--at-least-one-of', 'bin,postgres'];
