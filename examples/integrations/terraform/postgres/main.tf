9317b4bbca8a794449f8046d5d271e51411e69db
Iron-E <code.iron.e@gmail.com>
2024-04-12 19:16:47 -0400

    ci(terraform): postgres image

--- examples/integrations/terraform/postgres/main.tf
+++ examples/integrations/terraform/postgres/main.tf
@@ -12,3 +12,8 @@ terraform {
 resource "docker_image" "postgres" {
 	name = "postgres:16.2"
 }
+
+resource "docker_container" "winvoice-db-postgres" {
+	name = "winvoice-db-postgres"
+	image = docker_image.postgres
+}
