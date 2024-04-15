cd97401c8a7aa4e94b39dbda2bac6b99c78c2f92
Iron-E <code.iron.e@gmail.com>
2024-04-15 17:12:27 -0400

    ci(terraform): remove docker compose integration
    It requires swarm mode to be enabled

--- examples/integrations/docker-compose/terraform/postgres/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service" {
-	description = "The (winvoice db) postgres docker service"
-   value = docker_service.db
-}
--- examples/integrations/docker-compose/terraform/winvoice-server/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "container" {
-   value = docker_container.winvoice-server
-   description = "The winvoice server docker container"
-}
